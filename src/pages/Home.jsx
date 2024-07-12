edc7d7da44b21c67c337ea0848b274fd358acc9f
MustSel <mustafa.selcuk.tls@gmail.com>
2024-07-12 21:39:11 +0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import AddTutorial from "../components/AddTutorial";
+import TutorialList from "../components/TutorialList";
+import axios from "axios";
+
+const Home = () => {
+  const [tutorials, setTutorials] = useState([]);
+
+  const getTutorials = async () => {
+    try {
+      // const URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/"
+      const res = await axios.get(process.env.REACT_APP_URL);
+      console.log(res)
+      setTutorials(res.data.result.rows);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //? Mount asamasinda istek atilmali
+  useEffect(() => {
+    getTutorials();
+  }, []);
+
+  console.log(tutorials);
+  return (
+    <>
+      <AddTutorial getTutorials={getTutorials} />
+      <TutorialList tutorials={tutorials} getTutorials={getTutorials} />
+    </>
+  );
+};
+
+export default Home;
